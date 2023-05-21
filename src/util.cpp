aad07f0b8cf7b67fdbb7ed5a0b42f7da0e1af833
Oliver Lau <oliver@ersatzworld.net>
2023-05-21 15:46:37 +0200

    Added utility functions.

--- src/util.cpp
+++ src/util.cpp
@@ -29,4 +29,33 @@ namespace util
         return result.str();
     }
 
+    std::pair<std::string, std::string> unpair(const std::string &str, char delim)
+    {
+        std::pair<std::string, std::string> pair;
+        auto index = str.find(delim);
+        if (index != std::string::npos)
+        {
+            pair = std::make_pair(str.substr(0, index),
+                                  str.substr(index + 1, std::string::npos));
+        }
+        else
+        {
+            pair = std::make_pair(str, std::string());
+        }
+        return pair;
+    }
+
+    std::vector<std::string> split(const std::string &str, char delim)
+    {
+        std::vector<std::string> strings;
+        std::size_t start;
+        std::size_t end = 0;
+        while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+        {
+            end = str.find(delim, start);
+            strings.push_back(str.substr(start, end - start));
+        }
+        return strings;
+    }
+
 }
--- src/util.hpp
+++ src/util.hpp
@@ -1,8 +1,30 @@
 #pragma once
 
 #include <string>
+#include <sstream>
+#include <utility>
+#include <vector>
 
 namespace util
 {
-    extern std::string escaped(std::string const &str);
+    std::string escaped(std::string const &str);
+    std::vector<std::string> split(const std::string &str, char delim);
+    std::pair<std::string, std::string> unpair(const std::string &str, char delim);
+
+    template <typename InputIteratorT, typename SeparatorT>
+    std::string join(InputIteratorT input, SeparatorT separator)
+    {
+        std::ostringstream result;
+        auto i = std::begin(input);
+        if (i != std::end(input))
+        {
+            result << *i++;
+        }
+        while (i != std::end(input))
+        {
+            result << separator << *i++;
+        }
+        return result.str();
+    }
+
 }
